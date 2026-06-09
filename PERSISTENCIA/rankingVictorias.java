8600201c6cbdbadb715ae6cdef6871e9c62a0c72
alejandro871 <alejandro.barahona@correounivalle,edu,co>
2026-06-08 23:53:05 -0500

    ranking ordenado

--- PERSISTENCIA/rankingVictorias.java
+++ PERSISTENCIA/rankingVictorias.java
@@ -5,6 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
 
 public class rankingVictorias {
 
@@ -39,14 +43,16 @@ public class rankingVictorias {
             return;
         }
 
-        TreeMap<String, Integer> ranking = new TreeMap<>(victorias);
+        ArrayList<Map.Entry<String, Integer>> ranking = new ArrayList<>(victorias.entrySet());
+
+        Collections.sort( ranking, Comparator.comparing(Map.Entry<String, Integer>::getValue).reversed());
 
         System.out.println();
         System.out.println("===== RANKING =====");
+        
+        for (Map.Entry<String, Integer> entrada : ranking) {
 
-        for (String jugador : ranking.keySet()) {
-
-            System.out.println( jugador + " -> " + ranking.get(jugador) + " victorias" );
+            System.out.println( entrada.getKey() + " -> " + entrada.getValue() + " victorias");
         }
 
         System.out.println("===================");
